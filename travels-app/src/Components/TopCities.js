6754ef938609d3721bbe37e2eb7323a6aeb46559
Farha <khnfarha1987@gmail.com>
2023-06-25 19:38:54 +0100

    api-integration

--- travels-app/src/Components/TopCities.js
+++ travels-app/src/Components/TopCities.js
@@ -54,7 +54,7 @@ class CitySlider extends React.Component {
     console.log(topCitiesData);
     return (
       <Slider {...settings}>
-        {/* {topCitiesData &&
+        {topCitiesData &&
           topCitiesData.map(({ id, city, url, description, route }) => (
             <Link
               to={{ pathname: `/cities/${route}` }}
@@ -63,7 +63,7 @@ class CitySlider extends React.Component {
             >
               <CityCard city={city} url={url} description={description} />
             </Link>
-          ))} */}
+          ))}
       </Slider>
     );
   }
@@ -97,8 +97,74 @@ class CityCard extends Component {
 const topCitiesData = fetchCitiesFromResopnse();
 
 function fetchCitiesFromResopnse() {
-  return axios.get("https://restcountries.com/v3.1/region/America")
-    .then((response) => response.data);
+  var topCityArray = [];
+  getTopCitiesByPopulation()
+    .then(topCities => {
+      topCities.forEach(element => {
+        getCityImages(element.city).then(cityImages => {
+          console.log("iomage ", element.city, cityImages)
+          element.url = cityImages[0];
+        });
+        topCityArray.push(element)
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  console.log(topCityArray);
+  return topCityArray;
 }
+
+async function getTopCitiesByPopulation() {
+  const username = 'khnfarha1987'; // Replace with your GeoNames username
+
+  try {
+    const response = await axios.get('http://api.geonames.org/searchJSON', {
+      params: {
+        featureClass: 'P',
+        orderBy: 'population',
+        maxRows: 10, // Example: Get top 10 cities
+        username: username
+      }
+    });
+
+    const topCities = response.data.geonames.map(city => {
+      var url = '';
+      console.log("cityImage URL", url);
+      return {
+        id: city.adminCode1,
+        city: city.name,
+        url: url,
+        description: city.fcodeName,
+        route: city.name,
+      };
+    });
+    return topCities;
+  } catch (error) {
+    console.error('Error fetching top cities:', error);
+    throw error;
+  }
+}
+
+async function getCityImages(city) {
+  const unsplashApiKey = 'Jk7LUeRHwN6Jy9Zd5gczDzgVl6DphbAp1VMBcbr7SMk'; // Replace with your Unsplash API key
+
+  try {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: city,
+        client_id: unsplashApiKey
+      }
+    });
+
+    const images = response.data.results.map(result => result.urls.regular);
+    return images;
+  } catch (error) {
+    console.error('Error fetching city images:', error);
+    throw error;
+  }
+}
+
+
 
 export default TopCities;
